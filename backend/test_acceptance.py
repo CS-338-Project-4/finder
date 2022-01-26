5823c785e22a5c88997a9b9f11fc982fb47785d8
silversq <67875325+silversq@users.noreply.github.com>
2022-01-26 16:56:27 -0600

    Added 3 new tests

--- backend/test_acceptance.py
+++ backend/test_acceptance.py
@@ -27,3 +27,24 @@ def test3():
     answers = ['python', 'water', 'scissors', 'false']
     correct_answer = 'false'
     assert get_best_answer(types, answers) == correct_answer
+
+    
+def test4():
+    types = ['film director']
+    answers = ['Steven Spielberg', 'John Krasinski', 'Jack Black', 'Barack Obama']
+    correct_answer = 'Steven Spielberg'
+    assert get_best_answer(types, answers) == correct_answer
+
+    
+def test5():
+    types = ['water deity']
+    answers = ['Poseidon', 'Hades', 'Zeus', 'Hera']
+    correct_answer = 'Poseidon'
+    assert get_best_answer(types, answers) == correct_answer
+
+
+def test6():
+    types = ['mythological serpent']
+    answers = ['Python', 'kraken', 'Minotaur', 'Giant']
+    correct_answer = 'false'
+    assert get_best_answer(types, answers) == correct_answer
