3a18a8591006f9f648289685c737e2762bb088ad
Darryl Forbes <darrylforbes2022@u.northwestern.edu>
2022-01-27 22:18:44 -0600

    Update acceptance tests

--- backend/test_acceptance.py
+++ backend/test_acceptance.py
@@ -1,50 +1,51 @@
-from finder import get_best_answer
+from finder import get_answer
 
 
 def test0():
-    types = ['programming languages']
+    types = ['programming language']
     answers = ['python', 'water', 'scissors', 'false']
     correct_answer = 'python'
-    assert get_best_answer(types, answers) == correct_answer
+    assert get_answer(types, answers) == correct_answer
 
 
 def test1():
-    types = ['liquid']
+    types = ['environmental resource']
     answers = ['python', 'water', 'scissors', 'false']
     correct_answer = 'water'
-    assert get_best_answer(types, answers) == correct_answer
+    assert get_answer(types, answers) == correct_answer
 
 
 def test2():
+    """Test two levels up in ontology."""
     types = ['tool']
     answers = ['python', 'water', 'scissors', 'false']
     correct_answer = 'scissors'
-    assert get_best_answer(types, answers) == correct_answer
+    assert get_answer(types, answers) == correct_answer
 
 
 def test3():
     types = ['truth value']
     answers = ['python', 'water', 'scissors', 'false']
     correct_answer = 'false'
-    assert get_best_answer(types, answers) == correct_answer
+    assert get_answer(types, answers) == correct_answer
 
 
 def test4():
     types = ['film director']
-    answers = ['Steven Spielberg', 'John Krasinski', 'Jack Black', 'Barack Obama']
+    answers = ['John Krasinski', 'Jack Black', 'Steven Spielberg', 'Barack Obama']
     correct_answer = 'Steven Spielberg'
-    assert get_best_answer(types, answers) == correct_answer
+    assert get_answer(types, answers) == correct_answer
 
 
 def test5():
     types = ['water deity']
-    answers = ['Poseidon', 'Hades', 'Zeus', 'Hera']
+    answers = ['Hades', 'Poseidon', 'Zeus', 'Hera']
     correct_answer = 'Poseidon'
-    assert get_best_answer(types, answers) == correct_answer
+    assert get_answer(types, answers) == correct_answer
 
 
 def test6():
     types = ['mythological serpent']
-    answers = ['Python', 'kraken', 'Minotaur', 'Giant']
+    answers = ['kraken', 'Minotaur', 'Giant', 'Python']
     correct_answer = 'Python'
-    assert get_best_answer(types, answers) == correct_answer
+    assert get_answer(types, answers) == correct_answer
