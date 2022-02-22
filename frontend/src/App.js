5ed7ffe5acae5dea1d56a24d0394268e26c57d05
riya-s <91563940+riya-s@users.noreply.github.com>
2022-02-22 11:46:00 -0600

    Updating front end

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -37,7 +37,7 @@ function App() {
   console.log(getPossibleNodes());
   console.log(getScores());
 
-  return (
+  /* return (
 
   <div className="form-container">
 
@@ -73,7 +73,59 @@ function App() {
 
       </form>
     </div>
+  ); */
+
+  const [inputList, setInputList] = useState([{ options: "" }]);
+
+  // handle input change
+  const handleInputChange = (e, index) => {
+    const { name, value } = e.target;
+    const list = [...inputList];
+    list[index][name] = value;
+    setInputList(list);
+  };
+
+  // handle click event of the Remove button
+  const handleRemoveClick = index => {
+    const list = [...inputList];
+    list.splice(index, 1);
+    setInputList(list);
+  };
+
+  // handle click event of the Add button
+  const handleAddClick = () => {
+    setInputList([...inputList, { options: "" }]);
+  };
+
+  return (
+    <div className="form-container">
+      <Title />
+      {inputList.map((x, i) => {
+        return (
+          <div className="form-field">
+            <input
+              name="firstName"
+              placeholder="Enter First Name"
+              value={x.firstName}
+              onChange={e => handleInputChange(e, i)}
+            />
+            
+            <div className="form-container">
+              {inputList.length !== 1 && <button
+                className="form-field"
+                onClick={() => handleRemoveClick(i)}>Remove</button>}
+              {inputList.length - 1 === i && <button onClick={handleAddClick}>Add</button>}
+            </div>
+          </div>
+        );
+      })}
+      <div style={{ marginTop: 20 }}>{JSON.stringify(inputList)}</div>
+    </div>
   );
+
+
+
+
 }
 
 export default App;
