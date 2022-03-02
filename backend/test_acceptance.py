51672eb6124d5b6ed0174ac0e96337cbdb2bb0d4
Archit Chopra <archit.aryan@gmail.com>
2022-03-02 01:59:30 -0600

    added test cases 12-22

--- backend/test_acceptance.py
+++ backend/test_acceptance.py
@@ -90,10 +90,79 @@ def test11():
     correct_answer = 'Tom Cruise'
     assert get_answer(types, answers) == correct_answer
 
-
 def test12():
     """Test discovered bug in relation query score calculation."""
     types = ['lawyer', 'film director', 'film producer', 'actor']
     answers = ['Barack Obama', 'Steven Spielberg']
     correct_answer = 'Steven Spielberg'
     assert get_answer(types, answers) == correct_answer
+
+def test13():
+    """Test searching ambiguous answers."""
+    types = ['fruit']
+    answers = ['apple', 'strawberry']
+    correct_answer = 'apple'
+    assert get_answer(types, answers) == correct_answer
+
+def test14():
+    """Test searching ambiguous answers."""
+    types = ['actor']
+    answers = ['Q212064', 'Q175535']
+    correct_answer = 'Q212064'
+    assert get_answer(types, answers) == correct_answer
+
+# def test15(): TIMEOUT ERROR
+#     """Test searching same type / answer."""
+#     types = ['Q11424']
+#     answers = ['Q11424', 'Q1420']
+#     correct_answer = 'Q11424'
+#     assert get_answer(types, answers) == correct_answer
+
+def test16():
+    """Test searching one answer."""
+    types = ['Q1075']
+    answers = ['Q3142']
+    correct_answer = 'Q3142'
+    assert get_answer(types, answers) == correct_answer
+
+def test17():
+    """Test searching multiple of the same answer."""
+    types = ['color']
+    answers = ['orange', 'orange']
+    correct_answer = 'orange'
+    assert get_answer(types, answers) == correct_answer
+
+def test18():
+    """Normal test for type: film."""
+    types = ['Q11424', 'Q16144339']
+    answers = ['Q283', 'Q1420', 'Q26698156']
+    correct_answer = 'Q26698156'
+    assert get_answer(types, answers) == correct_answer
+
+# def test19(): CAR IS CHOSEN FOR TYPE ANIMAL
+#     """Normal test for type: animal."""
+#     types = ['animal']
+#     answers = ['car', 'tiger', 'Q22676']
+#     correct_answer = 'tiger'
+#     assert get_answer(types, answers) == correct_answer
+
+def test20():
+    """Second test for animal type with car as possible answer."""
+    types = ['animal']
+    answers = ['car', 'elephant', 'building']
+    correct_answer = 'elephant'
+    assert get_answer(types, answers) == correct_answer
+
+def test21():
+    """Normal test for type: animal."""
+    types = ['animal']
+    answers = ['lion', 'toy', 'building']
+    correct_answer = 'lion'
+    assert get_answer(types, answers) == correct_answer
+
+def test22():
+   """Normal test for type: mammal."""
+   types = ['mammal']
+   answers = ['car', 'tiger', 'toy']
+   correct_answer = 'tiger'
+   assert get_answer(types, answers) == correct_answer
